1998ebe1122510f7c268be91f4b37b27042bf85b
Jim Garlick <jgarlick@allegient.net>
2016-02-03 14:17:56 -0500

    Removed commented junk

--- init.rb
+++ init.rb
@@ -8,11 +8,6 @@ Redmine::Plugin.register :timesheet do
   author_url 'http://example.com/about'
 
   permission :timesheet, { :timesheet => [:index] }, :public => true
-#  project_module :timesheet do
-#    permission :timesheet, { :timesheet => [:index] }, :public => true
-#  end
-
-#  menu :project_menu, :timesheet, { :controller => 'timesheet', :action => 'index' }, :caption => 'Timesheet', :after => :activity, :param => :project_id
 
   menu :top_menu, :timesheet, { :controller => 'timesheet', :action => 'index' }, :caption => 'Timesheet'
 
